b584e2a1b29f40e11f8080613ef4528ca7066b38
rickchang <rickchang@wesure.cn>
2021-12-30 16:36:07 +0800

    01

--- 01_stack_heap/target/debug/deps/stack_heap.d
+++
@@ -1,5 +0,0 @@
-D:\data\rust\rust_learn\01_stack_heap\target\debug\deps\stack_heap.exe: src\main.rs
-
-D:\data\rust\rust_learn\01_stack_heap\target\debug\deps\stack_heap.d: src\main.rs
-
-src\main.rs:
--- 01_stack_heap/target/debug/stack_heap.d
+++
@@ -1,1 +0,0 @@
-D:\data\rust\rust_learn\01_stack_heap\target\debug\stack_heap.exe: D:\data\rust\rust_learn\01_stack_heap\src\main.rs
